6871b58d58d1f2cb9850f4bd57b7f4f547539248
Dawana Baker <krunk_011@hotmail.com>
2022-06-20 15:19:25 -0500

    adding

---
+++ dom_overview/dom_overview.js
@@ -0,0 +1,33 @@
+const user = {
+    name: 'Kali',
+    email: 'kali@devcamp.com',
+    favorites: [
+      'Chipotle',
+      'Freddys'
+    ]
+  }
+  
+  user.email
+  user.favorites[1]
+  
+  document
+  
+  document.location.href
+  
+  // On dailysmarty
+  document.location.protocol
+  
+  // On wsj
+  document.location.protocol
+  
+  document.body.parentElement
+  
+  document.getElementsByClassName('topics')
+  
+  document.getElementsByClassName('topics')[2]
+  
+  document.getElementsByClassName('topics')[2].children
+  
+  document.getElementsByClassName('topics')[2].children.length
+  
+  document.getElementsByClassName('topics')[2].children[0].text
