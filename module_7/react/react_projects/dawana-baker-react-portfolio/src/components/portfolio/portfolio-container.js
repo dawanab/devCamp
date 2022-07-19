609bc4dcd41c8ed3938cc188c95cce4d2c6d778a
dawanabaker <krunk_011@hotmail.com>
2022-07-18 22:04:07 -0500

    updating portfolio project

---
+++ module_7/react/react_projects/dawana-baker-react-portfolio/src/components/portfolio/portfolio-container.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import PortfolioItem from "./portfolio-item";
+
+import portfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Portfolio Items Go Here updating...</h2>
+
+				<PortfolioItem />
+			</div>
+		)
+	}
+}
