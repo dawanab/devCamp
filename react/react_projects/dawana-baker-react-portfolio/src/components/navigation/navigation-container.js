9b54f6cf6c64e159aba71d33ff33569796c0e470
dawanabaker <krunk_011@hotmail.com>
2022-08-16 00:29:01 -0500

    updating login routes

--- react/react_projects/dawana-baker-react-portfolio/src/components/navigation/navigation-container.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/navigation/navigation-container.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
+const NavigationComponent = (props) => {
 
-export default class NavigationComponent extends Component {
-    constructor() {
-        super();
-    }
-
-    render() {
+    const dynamicLink = (route, LinkText) => {
         return(
-            <div className="nav-wrapper">
-                <div className="left-side">
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-                    </div>
-            
-                    {/* {false ? <button>Add Blog</button> : null} */}
-                </div>
-
-                <div className="right-side">
-                    DAWANA BAKER
-                </div>
+            <div className="nav-link-wrapper">
+                <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
             </div>
         )
     }
+
+    
+
+    
+
+    return(
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                </div>
+
+                {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
+            </div>
+
+            <div className="right-side">DAWANA BAKER</div>
+        </div>
+    );
 }
+
+export default NavigationComponent;
