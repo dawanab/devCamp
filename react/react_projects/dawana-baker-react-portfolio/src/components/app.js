e47c1187e4148c68c265b7e6ca02f6fe982e52ce
dawanabaker <krunk_011@hotmail.com>
2022-08-16 23:56:58 -0500

    add portfolio manager component

--- react/react_projects/dawana-baker-react-portfolio/src/components/app.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/app.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
 import axios from 'axios';
 
-
 import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Blog from './pages/blog';
+import PortfolioManager from './pages/portfolio-manager';
 import PortfolioDetail from './portfolio/portfolio-detail';
 import Auth from './pages/auth';
 import NoMatch from "./pages/no-match";
-
-
 
 
 export default class App extends Component {
@@ -82,7 +80,9 @@ export default class App extends Component {
   }
 
   authorizedPages() {
-    return [<Route path="/blog" component={Blog} />]
+    return [
+        <Route path="/portfolio-manager" component={PortfolioManager} />
+      ]
   }
 
 
@@ -96,11 +96,8 @@ export default class App extends Component {
               handleSuccessfulLogout={this.handleSuccessfulLogout}
             />
 
-            <h2>{this.state.loggedInStatus}</h2>
-
             <Switch>
               <Route exact path="/" component={Home} />
-
               <Route 
                 path="/auth" 
                 render={props => (<Auth {...props} 
@@ -111,9 +108,9 @@ export default class App extends Component {
               />
 
               <Route path="/about-me" component={About} />
-
               <Route path="/contact" component={Contact} />
               {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
+              <Route path="/blog" component={Blog} />
               <Route exact path="/portfolio-detail/:slug" component={PortfolioDetail} />
               <Route component={NoMatch} />
             </Switch>
--- react/react_projects/dawana-baker-react-portfolio/src/components/navigation/navigation-container.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/navigation/navigation-container.js
@@ -8,7 +8,7 @@ const NavigationComponent = props => {
     const dynamicLink = (route, linkText) => {
         return(
             <div className="nav-link-wrapper">
-                <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                <NavLink to={route} activeClassName="nav-link-active">{linkText}</NavLink>
             </div>
         );
     };
@@ -41,7 +41,11 @@ const NavigationComponent = props => {
                     <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
                 </div>
 
-                {props.loggedInStatus === "LOGGED_IN" ? (dynamicLink("/blog", "Blog")) : null}
+								<div className="nav-link-wrapper">
+                    <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                </div>
+
+                {props.loggedInStatus === "LOGGED_IN" ? (dynamicLink("/portfolio-manager", "Portfolio Manager")) : null}
             </div>
 
             <div className="right-side">
