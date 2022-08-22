251466c1d173effdf78933cfa092cb6c5c0effd8
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2022-08-21 20:50:21 -0500

    update portfolio-manager

---
+++ react/react_projects/dawana-baker-react-portfolio/src/components/portfolio/portfolio-form.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+export default class PortfolioForm extends  Component {
+    render() {
+        return(
+            <div>
+                <h1>Portfolio Form...</h1>
+                <h4>Portfolio Form...</h4>
+            </div>
+        )
+    }
+}
