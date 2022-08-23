abc5848800e5accf7da381529910d837dc4223ab
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2022-08-23 00:27:47 -0500

    update portfolio-form

--- react/react_projects/dawana-baker-react-portfolio/src/components/portfolio/portfolio-form.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/portfolio/portfolio-form.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import React, { Component } from "react";
+import DropzoneComponent from "react-dropzone-component";
+
+import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
+import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
 
 export default class PortfolioForm extends  Component {
