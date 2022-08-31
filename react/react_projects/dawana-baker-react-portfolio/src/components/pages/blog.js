f5608ffe8ec6f5b2721ea1c8cf06b8125cd935f5
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2022-08-30 23:35:48 -0500

    update blog.js

--- react/react_projects/dawana-baker-react-portfolio/src/components/pages/blog.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/pages/blog.js
@@ -1,24 +1,44 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 class Blog extends Component {
 	constructor() {
-			super();
+		super();
+
+		this.state = {
+			blogItems: []
+		};
+
+		this.getBlogItems = this.getBlogItems.bind(this);
+	}
+
+	getBlogItems() {
+		axios.get("https://bakerdawana.devcamp.space/portfolio/portfolio_blogs",
+			{ withCredentials: true }
+			).then(response => {
+				this.setState({
+					blogItems: response.data.portfolio_blogs
+				});
+			}).catch(error => {
+				console.log("error", error);
+			});
+	}
+
+	componentWillMount() {
+		this.getBlogItems();
 	}
 
 	render() {
+		const blogRecords = this.state.blogItems.map(blogItem => {
+			return <h1>{blogItem.title}</h1>;
+		});
+
 		return(
 			<div>
-				<h2>Blog</h2>
-
-				<div>
-				<Link to="/about-me">Read more about me!</Link>
-				</div>
-
+				{blogRecords}
 			</div>
-		)
-			
-		
+		);
 	}
 }
 
