8b9fbde53fc7810236aa2c7a11eadb1f14ac07bb
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2022-09-03 06:21:11 -0500

    add api call for single blogItem

--- react/react_projects/dawana-baker-react-portfolio/src/components/pages/blog-detail.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/pages/blog-detail.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Blog detail</h1>
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentId: this.props.match.params.slug,
+			blogItem: {}
+		}
+	}
+
+	getBlogItems() {
+		axios.get(`https://bakerdawana.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+		).then(response => {
+			console.log("response", response)
+		}).catch(error => {
+			console.log("myError", error)
+		})
+	}
+
+	componentDidMount() {
+		this.getBlogItems();
+	}
+
+	render() {
+		console.log("currentId", this.state.currentId);
+		return(
+				<div>
+						<h1>Blog detail</h1>
+				</div>
+		)
+	}
 }
