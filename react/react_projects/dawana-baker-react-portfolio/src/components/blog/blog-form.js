cd8fac8a0f408dbd763ecff2b4419d6a61b2768d
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2022-09-15 07:48:56 -0500

    update blog form w/ rich text editor state

--- react/react_projects/dawana-baker-react-portfolio/src/components/blog/blog-form.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/blog/blog-form.js
@@ -9,17 +9,24 @@ export default class BlogForm extends Component {
 
         this.state = {
             title: "",
-            blog_status: ""
+            blog_status: "",
+            content: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+    }
+
+    handleRichTextEditorChange(content) {
+        this.setState({ content: content });
     }
 
     buildForm() {
         let formData = new FormData();
         formData.append("portfolio_blog[title]", this.state.title);
         formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+        formData.append("portfolio_blog[content]", this.state.content);
         return formData;
     }
 
@@ -71,7 +78,9 @@ export default class BlogForm extends Component {
                 </div>
 
                 <div className="one-column">
-                    <RichTextEditor />
+                    <RichTextEditor 
+                        handleRichTextEditorChange={this.handleRichTextEditorChange}
+                    />
                 </div>
 
                 <button className="btn">Save</button>
