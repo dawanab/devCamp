31fc3e17b243dc2dc14a990e569513172459d6f5
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2022-09-19 16:05:32 -0500

    Parsing HTML & Rendering to Blog Detail Page

--- react/react_projects/dawana-baker-react-portfolio/src/components/blog/blog-form.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/blog/blog-form.js
@@ -33,26 +33,28 @@ export default class BlogForm extends Component {
 		}
 
 		handleSubmit(event) {
-				axios.post("https://bakerdawana.devcamp.space/portfolio/portfolio_blogs", 
-										this.buildForm(),
-										{ withCredentials: true }
-								).then(response => {
-										// this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
-										this.setState({
-												title: "",
-												blog_status: "",
-												content: ""
-										});
+				axios
+				.post(
+					"https://bakerdawana.devcamp.space/portfolio/portfolio_blogs", 
+						this.buildForm(),
+						{ withCredentials: true }
+				)
+				.then(response => {
+					this.setState({
+							title: "",
+							blog_status: "",
+							content: ""
+					});
 
-										this.props.handleSuccessfulFormSubmission(
-												response.data.portfolio_blog
-										);
-								}).catch(error => {
-										console.log("handleSubmit for blog error", error);
-								});
+					this.props.handleSuccessfulFormSubmission(
+							response.data.portfolio_blog
+					);
+			})
+			.catch(error => {
+					console.log("handleSubmit for blog error", error);
+			});
 
-				// this.props.handleSuccessfulFormSubmission(this.state);
-				event.preventDefault();
+			event.preventDefault();
 		}
 
 		handleChange(event) {
