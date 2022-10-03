42dc0e7553f3dd4f6fb63c36caeb828079c13310
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2022-10-03 14:22:19 -0500

    rendering portfolio detail data to screen

--- react/react_projects/dawana-baker-react-portfolio/src/components/portfolio/portfolio-detail.js
+++ react/react_projects/dawana-baker-react-portfolio/src/components/portfolio/portfolio-detail.js
@@ -4,6 +4,10 @@ import axios from "axios";
 export default class PortfolioDetail extends Component  {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			portfolioItem: {}
+		}
 	}
 
 	componentWillMount() {
@@ -18,16 +22,30 @@ export default class PortfolioDetail extends Component  {
 			}`, 
 			{ withCredentials: true }
 			).then(response => {
-				console.log("res", response);
+				this.setState({
+					portfolioItem: response.data.portfolio_item
+		
+				})
 			}).catch(error => {
 				console.log("getportfolioItem error", error);
 			});
 	}
 
 	render() {
+		const {
+			name,
+			banner_image_url,
+			category,
+			description,
+			logo_url,
+			thumb_image_url,
+			url
+		} = this.state.portfolioItem;
+
 		return(
 			<div>
-				<h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+				<h2>{name}</h2>
+				<p>{description}</p>
 			</div>
 		)
 	}
